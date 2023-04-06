c680927f9d73ec52bf39022aa395652a71f532f1
adventuretimedi <olhadianaaa@gmail.com>
2023-04-06 12:06:36 +0300

    adding not found page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+// import { lazy, Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import GhostBox from './Pages/GhostBox';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route />
+        <Route />
+        <Route />
+        <Route path="*" element={<GhostBox />} />
+      </Routes>
     </div>
   );
 };
+
+export default App;

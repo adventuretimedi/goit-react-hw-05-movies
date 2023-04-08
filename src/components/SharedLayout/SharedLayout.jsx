94f1b77ef5dbcbf468aa0bf8f0b692997b535b79
adventuretimedi <olhadianaaa@gmail.com>
2023-04-08 16:22:05 +0300

    deploy fix

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -33,7 +33,6 @@ export const SharedLayout = () => {
                 height="80"
                 width="80"
                 ariaLabel="blocks-loading"
-                wrapperStyle={{}}
                 wrapperClass="blocks-wrapper"
                 colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
               />

8b1f1ebdb5aea1dbaa0b9806ce8ca4f84e9c04d3
adventuretimedi <olhadianaaa@gmail.com>
2023-04-11 18:22:32 +0300

    adding ghost

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,15 @@ const Movies = lazy(() => import('pages/Movies/Movies'));
 const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
 const Cast = lazy(() => import('./Cast/Cast'));
 const Reviews = lazy(() => import('./Reviews/Reviews'));
-// const GhostBox = lazy(() => import('../pages/NotFound/GhostBox'));
+const GhostBox = lazy(() => import('../pages/NotFound/GhostBox'));
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
+        {/* помітка для себе */}
+        <Route path="/goit-react-hw-05-movies" element={<Home />} />
+
         <Route index element={<Home />} />
         <Route path="movies" element={<Movies />} />
         <Route path="movies/:movieId" element={<MovieDetails />}>
@@ -20,8 +23,8 @@ export const App = () => {
           <Route path="reviews" element={<Reviews />} />
         </Route>
       </Route>
-      <Route path="*" element={<Navigate to="/" />} />
-      {/* <Route path="*" element={<GhostBox />} /> */}
+      {/* <Route path="*" element={<Navigate to="/" />} /> */}
+      <Route path="*" element={<GhostBox />} />
     </Routes>
   );
 };
